53958002308c3dc295885d5d7966102821fad29d
petterpaulo <46385095+petterpaulo@users.noreply.github.com>
2021-09-30 11:59:34 -0300

    Add files via upload

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Automabyte on 29/09/21.
+//  Copyright © 2021 Automabyte. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
